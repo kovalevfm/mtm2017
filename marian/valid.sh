6db092d5450f611c96b7f775c2c46e97da29e19c
Ubuntu <ubuntu@ip-172-31-42-59.ec2.internal>
2017-08-31 08:14:20 +0000

    fix validation script

--- marian/valid.sh
+++ marian/valid.sh
@@ -8,4 +8,5 @@ MODEL_NAME=baseline
 cat corpus.test.en \
    |  $MARIAN_PATH/build/amun \
         -c $MODEL_NAME.npz.amun.yml -b 12 -n --mini-batch 10 --maxi-batch 100 2>/dev/null \
-   | $MARIAN_PATH/examples/training/moses-scripts/scripts/generic/multi-bleu.perl corpus.test.pt
+   | $MARIAN_PATH/examples/training/moses-scripts/scripts/generic/multi-bleu.perl corpus.test.pt \
+   | cut -f 3 -d ' ' | cut -f 1 -d ','
